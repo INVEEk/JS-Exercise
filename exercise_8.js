35edd53876fe5b076f128a8bce05c380cf14efd5
Maciej <maciej.pieciuk@gmail.com>
2024-01-17 00:55:57 +0100

    Exercise 9 resolved

---
+++ exercise_8.js
@@ -0,0 +1,5 @@
+/*  6. Create two variables called width and height . Then, create a new variable that holds the area.
+Print it to the console
+ */
+
+let width, height;
---
+++ exercise_9.js
@@ -0,0 +1,12 @@
+/*
+7. Create two variables called firstNumber and secondNumber . Assign each of them with a
+different number. Write a program that swaps both values.
+*/
+
+let firstNumber = 1;
+    secondNumber = 2;
+
+[firstNumber, secondNumber] = [secondNumber, firstNumber];
+
+console.log(firstNumber);
+console.log(secondNumber);
