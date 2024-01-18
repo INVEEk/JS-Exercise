b4699b4b79f5b60af3ab100cdb007ae1cc8da706
Maciej <maciej.pieciuk@gmail.com>
2024-01-18 23:11:41 +0100

    Resolved exercise 17

---
+++ exercise_17.js
@@ -0,0 +1,19 @@
+function basicMathOperations(operation, value1, value2) {
+    if (operation == "+") {
+        return value1 + value2
+    }
+    if (operation == "-") {
+        return value1 - value2
+    }
+    if (operation == "*") {
+        return value1 * value2
+    }
+    if (operation == "/") {
+        return value1 / value2
+    }
+}
+
+console.log(basicMathOperations('+', 4, 7));
+console.log(basicMathOperations('-', 15, 18));
+console.log(basicMathOperations('*', 5, 5));
+console.log(basicMathOperations('/', 49, 7));
