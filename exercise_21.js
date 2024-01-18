ca522615473fa4b341bd583b614ff6e3b3326b3b
Maciej <maciej.pieciuk@gmail.com>
2024-01-19 00:33:01 +0100

    Exercise 21 resolved

--- exercise_21.js
+++ exercise_21.js
@@ -8,3 +8,17 @@ if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"
  */
+function bmi(weight, height) {
+    if ((weight / (height ** 2) <= 18.5)) {
+        return "Underweight";
+    } else if ((weight / (height ** 2) > 18.5) && (weight / (height ** 2) <= 25)) {
+        return "Normal";
+    } else if ((weight / (height ** 2) > 25) && (weight / (height ** 2) <= 30)) {
+        return "Overweight";
+    } else if (weight / (height ** 2) > 30) {
+        return "Obese";
+    }
+}
+
+console.log(bmi(70, 1.80));
+console.log(bmi(200, 2));
