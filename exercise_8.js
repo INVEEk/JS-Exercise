dfb67c8b627e1f47f56176130f63a08c531c0f67
Maciej <maciej.pieciuk@gmail.com>
2024-02-01 21:58:25 +0100

    Deleted old exercise

--- exercise_8.js
+++
@@ -1,8 +0,0 @@
-/*  6. Create two variables called width and height . Then, create a new variable that holds the area.
-Print it to the console
- */
-
-const height = 30;
-const width = 20;
-const area = height * width;
-console.log(area)
